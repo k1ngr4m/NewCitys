3b29015ea7cad378552135eb165f719ba0d7275d
k1ngr4m <365717424@qq.com>
2025-03-13 17:41:02 +0800

    fix: fixed some bugs

--- model/NewCity/NewCity.py
+++ model/NewCity/NewCity.py
@@ -366,7 +366,7 @@ class NewCity(nn.Module):
         self.patch_embedding_flow = PatchEmbedding_flow(
             d_model=args.embed_dim,
             patch_len=12,
-            stride=6,
+            stride=12,
             padding = None,
             his=args.input_window,
             num_feas=6  # 使用数据预处理中定义的维度
